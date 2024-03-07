10e802b5e28a8563759fe9725af2627a38eea25d
Flimill <uhovad03@gmail.com>
2024-03-08 00:20:06 +0300

    try 3

--- main.py
+++ main.py
@@ -10,6 +10,28 @@ models_number = 0
 
 activation_array = ['relu', 'sigmoid', 'tanh', 'softmax','leaky_relu']
 
+#пути к данным
+train_ds_path = 'training_dataset' 
+test_ds_path = 'testing_dataset' 
+models_path = 'models'  
+        
+#генерация новызх картинок
+img_width=64
+img_height=64
+num_samples_train= 5000#random.randint(4000, 5000)
+num_samples_test=1000
+min_fig_size = 10
+class_names = ['boxes', 'circles', 'treangle'] 
+#genererate_pictures(train_ds_path,num_samples_train,img_width,img_height,min_fig_size)
+#genererate_pictures(test_ds_path, num_samples_test,img_width,img_height,min_fig_size)
+        
+#тренировка модели
+num_classes = 3  # всего классов
+epochs = 20#random.randint(17, 25)  # Количество эпох
+batch_size = 64  # Размер мини-выборки
+learning_rate = 0.001#random.choice([0.01,0.001])
+num_layers = models_number
+
 for activation in activation_array:
     num_layers_array = []
     accuracy_array = []
@@ -18,37 +40,23 @@ for activation in activation_array:
     for num_layers in range(1,15):
         models_number+=1
         
-        #пути к данным
-        train_ds_path = 'training_dataset' 
-        test_ds_path = 'testing_dataset' 
-        models_path = 'models'  
-        
-        #генерация новызх картинок
-        img_width=64
-        img_height=64
-        num_samples_train= 5000#random.randint(4000, 5000)
-        num_samples_test=1000
-        min_fig_size = 10
-        class_names = ['boxes', 'circles', 'treangle'] 
-        #genererate_pictures(train_ds_path,num_samples_train,img_width,img_height,min_fig_size)
-        #genererate_pictures(test_ds_path, num_samples_test,img_width,img_height,min_fig_size)
-        
-        #тренировка модели
-        num_classes = 3  # всего классов
-        epochs = 20#random.randint(17, 25)  # Количество эпох
-        batch_size = 64  # Размер мини-выборки
-        learning_rate = 0.001#random.choice([0.01,0.001])
-        num_layers = models_number
+        #обучение модели
         name, num_neurons = train_model(train_ds_path,models_path,num_classes,epochs,batch_size,img_height, img_width,models_number,learning_rate, num_layers, activation)
         
         #тестирование модели
         test_accuracy3 = test_model(name, class_names, test_ds_path, img_width, img_height)
+        
+        
+        #заполнение массивов с данными
         num_layers_array.append(num_layers)
         accuracy_array.append(test_accuracy3)
         num_neurons_array.append(num_neurons)
+    
         print(num_layers_array)
         print(accuracy_array)
         print(num_neurons_array)
+        
+        #вывод информации о модели в консоль
         print(f'num_layers_array = {num_layers}')
         print(f'num_neurons = {num_neurons}')
         print(f'test_accuracy3 = {test_accuracy3}')   
--- plot_graph.py
+++ plot_graph.py
@@ -4,21 +4,25 @@ import matplotlib.pyplot as plt
 
 def plot_graph_layers(num_layers_array, accuracy_array, activation):
     # Построение графика точности от числа слоев
+    plt.figure()  # Создаем новое окно для графика
     plt.plot(num_layers_array, accuracy_array, marker='o')
     plt.title(f'Зависимость точности от числа слоев, для передаточной функции {activation}')
     plt.xlabel('Число слоев')
     plt.ylabel('Точность (accuracy)')
     plt.grid(True)
     plt.show(block=False)
-    plt.pause(5) 
+    plt.pause(5)
 
-def plot_graph_neurons(accuracy_array, num_neurons_array, activation):    
-    time.sleep(5)
-    # точности от числа нейронов
-    plt.plot(num_neurons_array, accuracy_array, marker='o')
-    plt.title(f'Зависимость точности от числа нейронов, для предаточной функции {activation}')
+def plot_graph_neurons(accuracy_array, num_neurons, activation):
+    time.sleep(5)    
+    # Создаем новое окно для графика
+    plt.figure()
+    
+    # Построение графика точности от числа нейронов
+    plt.plot(num_neurons, accuracy_array, marker='o')
+    plt.title(f'Зависимость точности от числа нейронов, для передаточной функции {activation}')
     plt.xlabel('Число слоев')
     plt.ylabel('Точность (accuracy)')
     plt.grid(True)
     plt.show(block=False)
-    plt.pause(5) 
+    plt.pause(5)
